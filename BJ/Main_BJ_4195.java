c994a23de3d5a3e8a7dfa99af33b01b6957888db
insunpark <37062271+insunpark@users.noreply.github.com>
2020-08-08 17:45:44 +0900

    Update Main_BJ_4195.java
    update 472ms

--- BJ/Main_BJ_4195.java
+++ BJ/Main_BJ_4195.java
@@ -26,25 +26,18 @@ public class Main_BJ_4195 {
 			parents = new int[200001]; // 친구 수 최대 범위
 			
 			for (int i = 0; i < parents.length; i++) {
-				parents[i] = i; // 초기화
-			}
-			
+				parents[i] = -1; // 초기화
+			}			
 			
 			int key;
-			int count;
+
 			for (int rel = 0; rel < F; rel++) {
 				st = new StringTokenizer(br.readLine()," ");
 				String s1 = st.nextToken();
 				String s2 = st.nextToken();
-				key = union(s1,s2); // 합집합
-				count = 0;
-				for (int i = 0; i < var; i++) {
-					if(key == find(parents[i])) { // 같은 집합인 개수 확인
-						count++;
-					}
-				}
-				sb.append(count).append('\n');
-			}			
+				key = -union(s1,s2); // 합집합				
+				sb.append(key).append('\n');
+			}
 		}// end for testcase
 		System.out.println(sb);
 	}// end main
@@ -64,18 +57,25 @@ public class Main_BJ_4195 {
 		
 		int aR = find(a);
 		int bR = find(b);
-		
-		if(aR == bR) return aR;		
-		else {
-			parents[bR] = aR;
-			return aR;
+				
+		// 여기서 리턴되는 부모노드의 값들은 전부 음수이다. 부모 노드들은 자식 노드의 개수를 절대값으로 가지고 있다
+		// 자식 노드의 개수가 적은 부모 노드를 자식 노드의 개수가 더 큰 부모 노드에 값을 더해 붙여 넣는다. 그 후 원래 부모노드였던 노드는 자식노드처럼 부모노드를 가르키게됨.
+		// 리턴값은 둘 중 부모노드가 된 값을 리턴해주면 사이즈 계산이 완료됨.
+		if(aR != bR) {
+			if(parents[aR] < parents[bR]) { 
+				parents[aR] += parents[bR];
+				parents[bR] = aR;
+			} else {
+				parents[bR] += parents[aR];
+				parents[aR] = bR;
+			}
 		}
-		
+		return parents[aR] < 0 ? parents[aR] : parents[bR];
 	}// end union
 	/** 부모찾아가는 과정 */
 	public static int find(int a) {
-		if(parents[a] == a) return a;
-		else return parents[a] = find(parents[a]);
+		if(parents[a] < 0) return a; // 부모 노드의 값이 음수니까 최종 부모노드에 도달하면
+		return parents[a] = find(parents[a]); 
 	}// end find
 	
 	
